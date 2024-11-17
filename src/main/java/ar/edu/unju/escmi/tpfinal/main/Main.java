a5d3c0d4006720d20b7ba64645f99e40229002b2
nahue <nahue@Nahuel>
2024-11-17 18:12:41 -0300

    Funciones de Cliente | Vilte Daniel Nahuel

--- src/main/java/ar/edu/unju/escmi/tpfinal/main/Main.java
+++ src/main/java/ar/edu/unju/escmi/tpfinal/main/Main.java
@@ -1,6 +1,9 @@
 package ar.edu.unju.escmi.tpfinal.main;
 
+import java.util.List;
 import java.util.Scanner;
+
+import javax.persistence.NoResultException;
 
 import ar.edu.unju.escmi.tpfinal.entities.Cliente;
 import ar.edu.unju.escmi.tpfinal.dao.IClienteDao;
@@ -22,8 +25,10 @@ public class Main {
 				registrarCliente();
 				break;
 			case 2:
+				mostrarClientes();
 				break;
 			case 3:
+				modificarCliente();
 				break;
 			case 4:
 				break;
@@ -152,11 +157,88 @@ public class Main {
 		String nombre = ingresarString( "Registrar el nombre del Cliente: ");
 		String apellido = ingresarString( "Registrar el apellido del Cliente: ");
 		String domicilio = ingresarStringConEspacios( "Registrar el domicilio del Cliente: ");
-		int dni = ingresarNumeroEntero( "Registrar el dni del Cliente: " );
+		String telefono = ingresarStringConEspacios( "Registrar el telefono del cliente");
+		Long dni = ingresarNumeroEnteroLargo( "Registrar el dni del Cliente: " );
 		
-		Cliente cliente = new Cliente(nombre, apellido, domicilio, dni, true);
+		Cliente cliente = new Cliente(dni, nombre, apellido, domicilio, telefono, true);
 		
 		clienteDao.guardarCliente(cliente);
 		
+	}
+	
+	public static void mostrarClientes() {
+		List<Cliente> clientes = clienteDao.obtenerClientes();
+		clientes.forEach(System.out::println);
+	}
+	
+public static void modificarCliente() {
+		
+		List<Cliente> clientes = clienteDao.obtenerClientes();
+		
+		if(clientes.stream().filter(c -> c.isEstado() == true).count() == 0) {
+			System.out.println("No hay clientes disponibles");
+			return;
+		}
+		
+		Cliente cliente = null;
+		boolean existe = false;
+		
+		clientes.forEach(cli -> {
+			if(cli.isEstado() == true) System.out.println(cli.getId()+") "+cli.getNombre()+ " "
+														+ cli.getApellido() + " "+ cli.getDni());
+		});
+		
+		long id = -1;
+		while(!existe) {
+			
+	        try {
+	        	id = ingresarNumeroEntero("Ingresar el id del cliente que desea modificar: ");
+				cliente = clienteDao.obtenerCliente(id);
+				existe=true;
+	        } catch (NoResultException e) {
+	            System.out.println("Cliente con ID " + id + " no encontrado.");
+	            throw e;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            System.out.println("Ocurrió un error al buscar el cliente.");
+	        }
+		}
+		
+		System.out.println("El cliente encontrado es: \n" + cliente.toString());
+		
+		int opcion = 0;
+		do {
+			System.out.println("\n1 - Modificar nombre.");
+			System.out.println("2 - Modificar apellido.");
+			System.out.println("3 - Modificar domicilio.");
+			System.out.println("4 - Modificar telefono.");
+			System.out.println("5 - Salir");
+			opcion = ingresarNumeroEntero( "Ingresar opcion: ");
+			switch (opcion) {
+			case 1 :
+				String nombre = ingresarString("Registrar el nuevo nombre del Cliente: ");
+				cliente.setNombre(nombre);
+				break;
+			case 2 :
+				String apellido = ingresarString("Registrar el nuevo apellido del Cliente: ");
+				cliente.setApellido(apellido);
+				break;
+			case 3 : 
+				String domiclio = ingresarStringConEspacios("Registrar el nuevo domiclio del Cliente: ");
+				cliente.setDomicilio(domiclio);
+				break;
+			case 4:
+				String telefono = ingresarStringConEspacios("Registrar el nuevo telefono del Cliente: ");
+				cliente.setTelefono(telefono);
+			case 5 :
+				System.out.println("Se finalizó la modicación");
+				break;
+			default:
+				System.out.println("Opción no válida");
+				break;
+			}
+		}while(opcion != 4);
+		
+		clienteDao.modificarCliente(cliente);
 	}
 }
