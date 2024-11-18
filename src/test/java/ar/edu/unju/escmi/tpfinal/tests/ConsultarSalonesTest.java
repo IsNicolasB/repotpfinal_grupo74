cdd9b91d66735d073ce20a0686243e1e71c267e2
PC <PC@MaxStreet-PC>
2024-11-18 15:00:50 -0300

    Se realiza Test Consultar Salones - Andreu

--- src/test/java/ar/edu/unju/escmi/tpfinal/tests/ConsultarSalonesTest.java
+++ src/test/java/ar/edu/unju/escmi/tpfinal/tests/ConsultarSalonesTest.java
@@ -2,6 +2,7 @@ package ar.edu.unju.escmi.tpfinal.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.unju.escmi.tpfinal.dao.ISalonDao;
@@ -12,21 +13,21 @@ import org.junit.jupiter.api.Test;
 import ar.edu.unju.escmi.tpfinal.entities.Salon;
 
 class ConsultarSalonesTest {
-	List<Salon> expected, expected2;
-	public static ISalonDao salonDao = new SalonDaoImp();
+	private List<Salon> expected;
+	private ISalonDao salonDao = new SalonDaoImp();
 	@BeforeEach
 	void setUp() throws Exception {
+		expected=new ArrayList<Salon>();
 		Salon test1 = new Salon("Test1", 20, true, 10000.0);
 		salonDao.guardarSalon(test1);
 		expected.add(test1);
 		Salon test2 = new Salon("Test2", 20, true, 10000.0);
 		salonDao.guardarSalon(test2);
 		expected.add(test2);
-		List<Salon> expected2=salonDao.obtenerSalones();
 	}
 
 	@Test
 	void testConsultarSalones() {
-		assertEquals(expected, expected2);
+		assertEquals(expected, salonDao.obtenerSalones(),"Los salones no se guardaron correctamente");
 	}
 }
