513d1a3709ba04f1ffe2ffb5d0678afe7daeb620
nahue <nahue@Nahuel>
2024-11-17 21:21:23 -0300

    Agregado de Exceptions en Test | Vilte Daniel

--- src/main/java/ar/edu/unju/escmi/tpfinal/dao/IClienteDao.java
+++ src/main/java/ar/edu/unju/escmi/tpfinal/dao/IClienteDao.java
@@ -6,7 +6,7 @@ import ar.edu.unju.escmi.tpfinal.entities.Cliente;
 import ar.edu.unju.escmi.tpfinal.exceptions.ClienteNoEncontradoException;
 
 public interface IClienteDao {
-	abstract void guardarCliente(Cliente cliente);
+	void guardarCliente(Cliente cliente);
 	void modificarCliente(Cliente cliente);
 	Cliente obtenerCliente(Long clienteId) throws ClienteNoEncontradoException;
 	List<Cliente> obtenerClientes();
