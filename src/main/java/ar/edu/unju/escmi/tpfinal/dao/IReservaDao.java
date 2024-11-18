0e34fad13c6ec41503986f7ca9f83aa9dd627dc7
IsNicolasB <isma.nico7842018@gmail.com>
2024-11-17 21:12:21 -0300

    Dao e implementaciones de Reserva - Burgos Nicolás

---
+++ src/main/java/ar/edu/unju/escmi/tpfinal/dao/IReservaDao.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.escmi.tpfinal.dao;
+
+import java.util.List;
+
+import ar.edu.unju.escmi.tpfinal.entities.Reserva;
+
+public interface IReservaDao {
+	void guardarReserva(Reserva reserva);
+	Reserva obtenerReserva(Long id);
+	List<Reserva> obtenerReservas();
+}
